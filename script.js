7a81e2ffe700531eb92700cc7c31a59dfdb6dc1c
CaetanoY <125322239+CaetanoY@users.noreply.github.com>
2023-11-27 20:30:40 -0300

    Trabalho A3-2023

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("Automação residencial").addEventListener("click", function() {
+    // Redireciona para outra página HTML
+    window.location.href = "Automacao-residencial/Residencial.html";
+});
+
+document.getElementById("Automação industrial").addEventListener("click", function() {
+    // Redireciona para outra página HTML
+    window.location.href = "Automacao-industrial/Industrial.html";
+});
+
+document.getElementById("Automação na saúde").addEventListener("click", function() {
+    // Redireciona para outra página HTML
+    window.location.href = "Atomacao-na-saude/Saude.html";
+});
+
